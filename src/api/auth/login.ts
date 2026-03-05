57fbe77a3f6ab6fb996ca0bd45a1038670d933ca
ayush ahirwar <ayushahirwar04@gmail.com>
2026-03-05 18:23:29 +0530

    login api added

--- src/api/auth/login.ts
+++ src/api/auth/login.ts
@@ -1,1 +1,9 @@
-// export 
+import api from "@/config/axios"
+import { log } from "console";
+
+export const login=async(data:FormData)=>{
+    const response = await api.post("/api/auth/login",data);
+    console.log(response.data);
+    return response.data;
+    
+}
