91642db9a3b7a34b420748fa306d13192d83adea
ayush ahirwar <ayushahirwar04@gmail.com>
2026-03-05 18:07:42 +0530

    added authProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Wrapper from "@/components/hoc/Wrapper";
+import AuthProvider from "@/components/hoc/AuthProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Wrapper>
+          <AuthProvider>{children}</AuthProvider>
+        </Wrapper>
       </body>
     </html>
   );
