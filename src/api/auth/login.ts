6af1e383d583cebadbee405408684541d0177d12
ayush ahirwar <ayushahirwar04@gmail.com>
2026-03-06 15:48:50 +0530

    fixed the signin bugs

--- src/api/auth/login.ts
+++ src/api/auth/login.ts
@@ -1,5 +1,6 @@
 import api from "@/config/axios"
-import { log } from "console";
+
+
 
 export const login = async(data:FormData)=>{
     const response = await api.post("/api/auth/login",data);
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,21 +9,22 @@ export type User = {
   id: string;
   name: string;
   email: string;
-  role: Role;
   isVerified:boolean
 };
 
 const JWT_SECRET = process.env.JWT_SECRET!;
-console.log(JWT_SECRET)
 if (!JWT_SECRET) {
   throw new Error('JWT_SECRET is not set in environment variables');
 }
 
 
 export async function getCurrentUser(): Promise<User | null> {
+  
   try {
-    const cookieStore = await cookies();
+    const cookieStore =await cookies();
     const token = cookieStore.get("access")?.value;
+    console.log(token);
+    
     if (!token) {
       console.log("No token found");
       return null;
