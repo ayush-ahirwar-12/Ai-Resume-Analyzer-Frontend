ea3f43146ef5f8eb19eaedf53e843a9ce5b414f4
ayush ahirwar <ayushahirwar04@gmail.com>
2026-03-06 15:56:38 +0530

    signup form layout added

--- src/api/auth/login.ts
+++ src/api/auth/login.ts
@@ -1,7 +1,5 @@
 import api from "@/config/axios"
 
-
-
 export const login = async(data:FormData)=>{
     const response = await api.post("/api/auth/login",data);
     console.log(response.data);
--- src/features/auth/hooks/useAuthApi.tsx
+++ src/features/auth/hooks/useAuthApi.tsx
@@ -1,6 +1,14 @@
 import * as api from "@/api"
 import { useMutation } from "@tanstack/react-query"
 
+export const useRegisterApi = () => {
+    return useMutation({
+        mutationKey:["register"],
+        mutationFn: (data:FormData) => api.register(data);
+        retry:0
+    })
+}
+
 export const useLogin = () => {
     return useMutation({
         mutationKey: ["login"],
@@ -8,3 +16,4 @@ export const useLogin = () => {
         retry: 0
     })
 }
+
