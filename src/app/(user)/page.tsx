acf3860ccf4584784d0f5cc88699d32f751f6dfd
ayush ahirwar <ayushahirwar04@gmail.com>
2026-03-07 18:57:05 +0530

    homepage created

---
+++ src/app/(user)/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from '@/components/ui/Navbar'
+import Home from '@/features/home/Home'
+import React from 'react'
+
+export default function UserHomePage(){
+
+    return(
+        <div>
+        <Navbar/>
+        <Home/>
+        </div>
+    ) 
+}
+
